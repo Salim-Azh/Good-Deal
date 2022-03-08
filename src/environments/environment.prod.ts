148f29dcfc388befe19909743dee3cb5ade44f5c
Salim Azharhoussen <azharhoussen.salim@gmail.com>
2022-03-08 01:33:09 -0500

    fix missing config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCpsrRGBdLDHGKLknSLc35ud4nHyAooRVM",
+    authDomain: "good-deal-f8d6d.firebaseapp.com",
+    projectId: "good-deal-f8d6d",
+    storageBucket: "good-deal-f8d6d.appspot.com",
+    messagingSenderId: "334441911803",
+    appId: "1:334441911803:web:26c34e3aa654733b8be75e"
+  }
 };
