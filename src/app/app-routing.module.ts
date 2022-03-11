38284c10b1f35870b85cf11f5935212b82a2458a
Salim Azharhoussen <azharhoussen.salim@gmail.com>
2022-03-11 00:45:47 -0500

    action bar routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './messages/messages.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PublishComponent } from './publish/publish.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'publish', component: PublishComponent},
+  {path: 'messages', component: MessagesComponent},
+  {path: 'account', component: AccountComponent},
+  {path: '**', component: PageNotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,25 @@ import { provideMessaging,getMessaging } from '@angular/fire/messaging';
 import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
 import { provideStorage,getStorage } from '@angular/fire/storage';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
+import { FooterMobileComponent } from './footer-mobile/footer-mobile.component';
+import { HomeComponent } from './home/home.component';
+import { PublishComponent } from './publish/publish.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AccountComponent } from './account/account.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterMobileComponent,
+    HomeComponent,
+    PublishComponent,
+    MessagesComponent,
+    AccountComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +44,8 @@ import { provideStorage,getStorage } from '@angular/fire/storage';
       registrationStrategy: 'registerWhenStable:30000'
     }),
     BrowserAnimationsModule,
+    MatToolbarModule,
+    AngularSvgIconModule.forRoot(),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
