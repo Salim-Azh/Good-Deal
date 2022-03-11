d127cbc748aeca634d4f66ae89c3d99803bb249b
audreylemarouille <audrey.lemarouille07@gmail.com>
2022-03-11 18:33:43 -0500

    maj de l'interface ads pour tester l'affichage

--- src/app/components/ads/ads.ts
+++ src/app/components/ads/ads.ts
@@ -2,15 +2,8 @@ import { Timestamp } from "firebase/firestore";
 
 export interface Ads {
     id?: string;
-    advertiserName : string;
-    category: string;
-    createdAt: Timestamp;
-    description: string;
-    imagesUrl: string;
-    latitude: number;
-    longitude: number;
+    imageUrl: string;
+    title: string;
     price: number;
     residenceName: string;
-    state: string;
-    title: string;
 }
