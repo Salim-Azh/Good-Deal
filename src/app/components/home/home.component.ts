9c88e9113b0102d38eed46c8420d0ca6188e709d
audreylemarouille <audrey.lemarouille07@gmail.com>
2022-03-11 18:34:17 -0500

    afficher liste ads dans home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Ads } from '../ads/ads';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  recherche: Ads[] = [
+    {
+      imageUrl: '../../../assets/img/pc.jpeg', 
+      title: 'Ordinateur portable', 
+      price:200, 
+      residenceName:'Résidence UQAM'
+    },
+    {
+      imageUrl: '../../../assets/img/pc.jpeg', 
+      title: 'Ordinateur portable', 
+      price:200, 
+      residenceName:'Résidence UQAM'
+    },{
+      imageUrl: '../../../assets/img/pc.jpeg', 
+      title: 'Ordinateur portable', 
+      price:200, 
+      residenceName:'Résidence UQAM'
+    }
+  ]
+
 
 }
