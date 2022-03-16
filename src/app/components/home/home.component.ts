c4468ea70249537d950a212b6a55ac2e3466e6a0
Salim Azharhoussen <azharhoussen.salim@gmail.com>
2022-03-16 03:24:31 -0400

    fix default results multiple residences

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -18,7 +18,7 @@ import { Ad } from 'src/app/model/ad.model';
 export class HomeComponent implements OnInit {
 
   residences: Residence[] = [];
-  ads: Ad[]= [];
+  ads: Ad[] = [];
 
   constructor(private firestore: Firestore) { }
 
@@ -26,7 +26,7 @@ export class HomeComponent implements OnInit {
 
     const querySnapshot = await this.getResidences();
 
-    querySnapshot.docs.forEach((doc) => {
+    querySnapshot.docs.forEach(async (doc) => {
       // doc.data() is never undefined for query doc snapshots
       //console.log(doc.id, " => ", doc.data());
       if (doc.exists()) {
@@ -37,43 +37,41 @@ export class HomeComponent implements OnInit {
           latitude: doc.get('latitude'),
           longitude: doc.get('longitude'),
         } as Residence)
+        const querySnapshot2 = await this.getResidencesAdsById(doc.id)
+        querySnapshot2.docs.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          //console.log(doc.id, " => ", doc.data());
+          if (doc.exists()) {
+            this.ads.push({
+              id: doc.id,
+              advertiser: doc.get('advertiser'),
+              advertiserName: doc.get('advertiserName'),
+              category: doc.get('category'),
+              createdAt: doc.get('createdAt'),
+              deal: doc.get('deal'),
+              description: doc.get('description'),
+              imagesUrl: doc.get('imagesUrl'),
+              latitude: doc.get('latitude'),
+              longitude: doc.get('longitude'),
+              price: doc.get('price'),
+              residenceName: doc.get('residenceName'),
+              state: doc.get('state'),
+              title: doc.get('title'),
+            } as Ad)
+          }
+
+
+        });
       }
-
-
-    });
-
-    const querySnapshot2 = await this.getResidencesAdsById(this.residences[0].id)
-    querySnapshot2.docs.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      //console.log(doc.id, " => ", doc.data());
-      if (doc.exists()) {
-        this.ads.push({
-          id: doc.id,
-          advertiser:doc.get('advertiser'),
-          advertiserName:doc.get('advertiserName'),
-          category:doc.get('category'),
-          createdAt:doc.get('createdAt'),
-          deal:doc.get('deal'),
-          description:doc.get('description'),
-          imagesUrl:doc.get('imagesUrl'),
-          latitude:doc.get('latitude'),
-          longitude:doc.get('longitude'),
-          price:doc.get('price'),
-          residenceName:doc.get('residenceName'),
-          state:doc.get('state'),
-          title:doc.get('title'),
-        } as Ad)
-      }
-
 
     });
   }
 
-  async getResidences(){
+  async getResidences() {
     return getDocs(collection(this.firestore, "residences"));
   }
 
-  async getResidencesAdsById(id: any){
-    return getDocs(collection(this.firestore, "residences/"+id+"/ads"));
+  async getResidencesAdsById(id: any) {
+    return getDocs(collection(this.firestore, "residences/" + id + "/ads"));
   }
 }
