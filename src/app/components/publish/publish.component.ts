cfa34e0e04725484e2e30d9fa27262547c5389fa
bervic <nouzadjeubervic@yahoo.fr>
2022-04-04 06:38:00 +0200

    upload image GD029 version 2

--- src/app/components/publish/publish.component.ts
+++ src/app/components/publish/publish.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { Firestore, getFirestore, collection, addDoc, getDoc, doc, Timestamp, updateDoc } from "firebase/firestore";
+import { Firestore } from '@angular/fire/firestore';
+import {  getFirestore, collection, addDoc, getDoc, doc, Timestamp, updateDoc } from "firebase/firestore";
 import { Ad } from 'src/app/model/ad.model';
 import { getAuth } from 'firebase/auth';
 import { User } from 'src/app/model/user.model';
@@ -56,14 +57,15 @@ export class PublishComponent implements OnInit {
   //public file: any = {}
   @Input() file: File;
 
+
   constructor(
     public authService: AuthService,
     private userService: UserService,
     private adService: AdService,
     private uploadfileService: UploadfileService,
     private af: AngularFireStorage,
+    private firestore: Firestore,
     public storage: Storage
-
 
   ) {
     this.user = new User();
@@ -88,12 +90,30 @@ export class PublishComponent implements OnInit {
 
   ) {
     if (this.user) {
-      this.adService.createAd(title, category, price, description, state);
+      // Upload file and metadata to the object 'images/mountains.jpg'
+
+    this.adService.createAd(title, category, price, description, state);
+
     }
   }
 
+  uploadImage(){
+    console.log(this.file);
 
-  TakePhoto(FormimagesUrl: HTMLInputElement, user, event : any) {
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    const storageRef = ref(this.storage, 'Images/' + this.file.name);
+    const uploadTask = uploadBytesResumable(storageRef, this.file);
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.then(snapshot => {
+      return getDownloadURL(snapshot.ref)
+    }).then(downloadURL => {
+      console.log('downloadURL', downloadURL)
+    })
+    //this.af.upload("Images/"+Math.random()+this.patch, this.patch)
+
+      }
+
+  TakePhoto(FormimagesUrl: HTMLInputElement, _user, event : any) {
     this.file = event.target.files[0]
 
     this.photoSourceObj = FormimagesUrl.files[0];
@@ -104,7 +124,7 @@ export class PublishComponent implements OnInit {
       const reader = new FileReader();
       reader.readAsDataURL(this.photoSourceObj);
 
-      reader.onload = (e) => {
+      reader.onload = (_e) => {
         const imageUrl = reader.result;
 
         this.photoReadersubject.next([...this.photoReadersubject.getValue(), imageUrl]);
@@ -134,54 +154,7 @@ export class PublishComponent implements OnInit {
   }
 
 
-  uploadImage(){
-console.log(this.file);
-// Upload file and metadata to the object 'images/mountains.jpg'
-const storageRef = ref(this.storage, 'Images/' + this.file.name);
-const uploadTask = uploadBytesResumable(storageRef, this.file);
-// Listen for state changes, errors, and completion of the upload.
-uploadTask.on('state_changed',
-  (snapshot) => {
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-      case 'paused':
-        console.log('Upload is paused');
-        break;
-      case 'running':
-        console.log('Upload is running');
-        break;
-    }
-  },
-  (error) => {
-    // A full list of error codes is available at
-    // https://firebase.google.com/docs/storage/web/handle-errors
-    switch (error.code) {
-      case 'storage/unauthorized':
-        // User doesn't have permission to access the object
-        break;
-      case 'storage/canceled':
-        // User canceled the upload
-        break;
 
-      // ...
-
-      case 'storage/unknown':
-        // Unknown error occurred, inspect error.serverResponse
-        break;
-    }
-  },
-  () => {
-    // Upload completed successfully, now we can get the download URL
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      console.log('File available at', downloadURL);
-    });
-  }
-);
-//this.af.upload("Images/"+Math.random()+this.patch, this.patch)
-
-  }
 
   /*upload($event) {
     this.patch = $event.target.files[0]
