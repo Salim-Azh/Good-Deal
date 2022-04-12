ce4ce8ff81bef9d498875d51aa0c54c2a0baec89
Salim Azharhoussen <azharhoussen.salim@gmail.com>
2022-04-12 13:51:37 -0400

    ad details update

--- src/app/components/ad-details/ad-details.component.ts
+++ src/app/components/ad-details/ad-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { format } from 'path';
 import { Ad } from '../../model/ad.model';
 //import { GalleryItem, ImageItem } from 'ng-gallery';
 
@@ -11,18 +12,54 @@ export class AdDetailsComponent implements OnInit {
 
   @Input() ad!: Ad;
 
+  displayState: string;
+  displayDate: string;
+
+  constructor() {
+    this.displayState = "";
+    this.displayDate = "";
+  }
   /**
    * images:  GalleryItem[] = [];
    * thumbHeight=0;
    */
-  
+
 
   ngOnInit() {
 
     /**
      * this.images = [new ImageItem({ src: this.ad.imagesUrl})];
      */
-    
+
+    this.setDisplayState();
+    this.setDisplayDate();
+
+  }
+
+  private setDisplayState(){
+    if (this.ad.state == "new") {
+      this.displayState = "Neuf";
+    }
+    else if (this.ad.state == "like new") {
+      this.displayState = "Usagé - Comme Neuf";
+    }
+    else if (this.ad.state == "used") {
+      this.displayState = "Usagé";
+    }
+  }
+
+  private setDisplayDate() {
+    const date = this.ad.createdAt.toDate();
+
+    let month = date.getMonth().toString();
+    let day = date.getDay().toString();
+    if (month.length == 1) {
+      month = `0${month}`;
+    }
+    if (day.length == 1) {
+      day = `0${day}`;
+    }
+    this.displayDate = `${date.getFullYear()}-${month}-${day}`
   }
 
 }
